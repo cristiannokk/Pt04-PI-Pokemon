e579a14b8c36613f8902de2cb6eb4e43b85318b7
cristiannokk <83661346+cristiannokk@users.noreply.github.com>
2022-04-07 23:02:40 -0300

    Update Pokemon.js
    Se termino el models de Pokemon

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    ID: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    stat: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+    },
+    weight:{
+      type: DataTypes.INTEGER,
+    }
+  },{ 
+    timestamps: false 
   });
 };
+
+
+// ID (Número de Pokemon) * : No puede ser un ID de un pokemon ya existente en la API pokeapi
+// Nombre *-
+// Vida -
+// Fuerza
+// Defensa
+// Velocidad
+// Altura
+// Peso
