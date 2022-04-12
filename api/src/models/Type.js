14e8abb04f4f4b7986088282d30c85b1231d742f
cristiannokk <83661346+cristiannokk@users.noreply.github.com>
2022-04-12 20:41:31 -0300

    Update Type.js
    update defaultValue ---> 'normal'

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -14,7 +14,8 @@ module.exports = (sequelize) => {
     },
     name: {
       type: DataTypes.STRING,
-      defaultValue: 'unknown'
+      defaultValue: 'normal',
+      allowNull: true,
     }
   },
   { 
